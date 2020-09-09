06e7d4cf9f25c42a1059858037f7a0cb3b165702
ANKESH <ankeshsharma@protonmail.com>
2020-09-10 03:02:10 +0530

    Removed pic from background

--- Guis/main_frame.py
+++ Guis/main_frame.py
@@ -10,9 +10,6 @@ def main_frame(self,Email):
     self.geometry("1200x900")
     self.minsize(980, 660)
     self.config(bg="black")
-    img = PhotoImage(file="../res/canvalogo1.png")
-    label = Label(self, image=img)
-    label.place(x=5, y=-1)
     lb = Label(self, text="WELCOME {0}".format(Email.split("@")[0].upper()),  bg="black", fg="white",
                font=("comicsansms",30,"bold"),relief=FLAT).place(x=140, y=300)
     self.mainloop()
